38ee1e961046077a309d02d0d6d4993f5e187a8a
theeWaweru <davidngari47@gmail.com>
2023-09-20 17:01:34 +0300

    third commit with all error especially webpack sorted

--- app/shared-metadata.js
+++ app/shared-metadata.js
@@ -1,4 +1,4 @@
-// export const openGraphImage = { images: ["http://..."] };
+export const openGraphImage = { images: ["http://..."] };
 // openGraph: {
 //     ...openGraphImage,
 //     title: "theeWaweru | Creative Developer",
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    // Important: return the modified config
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

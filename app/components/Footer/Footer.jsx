8b6908fa8f47f0892725024fd5053a6dfefa2e27
davidngari47@gmail.com <davidngari47@gmail.com>
2025-05-06 15:59:11 +0300

    ninteenth commit for my website

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -25,53 +25,8 @@ const Footer = () => {
                             </li>
                         </ul>
                         <div className={styles.footerSocial}>
-                            <a
-                                href="https://github.com/theeWaweru"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={styles.footerSocialLink}
-                                aria-label="GitHub"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="20"
-                                    height="20"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                >
-                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                                </svg>
-                            </a>
-                            {/* <a
-                                href="https://twitter.com/theeWaweru"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={styles.footerSocialLink}
-                                aria-label="Twitter"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="20"
-                                    height="20"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                >
-                                    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-                                </svg>
-                            </a> */}
-                            <a href="Nairobi" className={styles.footerSocialLink}>
-                                <Icons.Map size={20} />
-                            </a>
-                            <a href="Nairobi" className={styles.footerSocialLink}>
-                                <Icons.X size={20} />
+                            <a href="https://github.com/theeWaweru" className={styles.footerSocialLink}>
+                                <Icons.Github size={20} />
                             </a>
                             <a href="https://www.linkedin.com/in/waweru-ngari/" className={styles.footerSocialLink}>
                                 <Icons.Linkedin size={20} />
@@ -89,27 +44,27 @@ const Footer = () => {
                         <ul className={styles.footerNavList}>
                             <li className={styles.footerNavItem}>
                                 <Link href="/" className={styles.footerNavLink}>
-                                    Home
+                                    Welcome
                                 </Link>
                             </li>
                             <li className={styles.footerNavItem}>
                                 <Link href="/work" className={styles.footerNavLink}>
-                                    Work
+                                    From the Lab
                                 </Link>
                             </li>
                             <li className={styles.footerNavItem}>
                                 <Link href="/blog" className={styles.footerNavLink}>
-                                    Blog
+                                    Mind Bytes
                                 </Link>
                             </li>
                             <li className={styles.footerNavItem}>
                                 <Link href="/about" className={styles.footerNavLink}>
-                                    About
+                                    Meet Waweru
                                 </Link>
                             </li>
                             <li className={styles.footerNavItem}>
                                 <Link href="/contact" className={styles.footerNavLink}>
-                                    Contact
+                                    Wanna Chat?
                                 </Link>
                             </li>
                         </ul>
